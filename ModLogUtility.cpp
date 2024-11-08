23d2caa7c53539f05ece278ddbd7e8f3560d9a93
Beef <84393940+TheRealBeef@users.noreply.github.com>
2024-11-09 00:39:10 +0100

    Initial Commit

---
+++ ModLogUtility.cpp
@@ -0,0 +1,130 @@
+﻿#include "ModLogUtility.h"
+#include "HAL/IConsoleManager.h"
+#include "HAL/FileManager.h"
+#include "Misc/OutputDevice.h"
+#include "Misc/Paths.h"
+#include "Misc/DateTime.h"
+
+DEFINE_LOG_CATEGORY(MODLOG_CATEGORY_NAME);
+
+static TAutoConsoleVariable<int32> CVarLogLevel(
+	TEXT(MODLOG_CONSOLE_VARIABLE_NAME), 1,
+	TEXT("Sets log verbosity level: 0 = Error, 1 = Warning, 2 = Debug"),
+	ECVF_Default
+);
+
+FModLogUtility FModLogUtility::Instance;
+
+FModLogUtility& FModLogUtility::Get()
+{
+	return Instance;
+}
+
+void FModLogUtility::InitializeLog()
+{
+	if (bUseCustomLogFile)
+	{
+		FScopeLock Lock(&LogFileMutex);
+		if (LogFilePath.IsEmpty())
+		{
+			LogFilePath = FPaths::Combine(
+				FPlatformMisc::GetEnvironmentVariable(TEXT("LOCALAPPDATA")),
+				TEXT(MODLOG_DIRECTORY),
+				*FString::Printf(TEXT("%s.log"), MODLOG_MOD_NAME)
+			);
+
+			IFileManager::Get().Delete(*LogFilePath);
+			RawLogMessage(FString::Printf(TEXT("%s Log Startup"), MODLOG_MOD_NAME));
+		}
+	}
+}
+
+void FModLogUtility::ShutdownLog()
+{
+	if (bUseCustomLogFile && !LogFilePath.IsEmpty())
+	{
+		RawLogMessage(TEXT("Module Log Shutdown"));
+		LogFilePath.Empty();
+	}
+}
+
+void FModLogUtility::SetUseCustomLogFile(bool bEnableCustomLogFile)
+{
+	bUseCustomLogFile = bEnableCustomLogFile;
+}
+
+void FModLogUtility::RawLogMessage(const FString& Message)
+{
+	FScopeLock Lock(&LogFileMutex);
+	if (!LogFilePath.IsEmpty())
+	{
+		FString FormattedMessage = Message + LINE_TERMINATOR;
+		FFileHelper::SaveStringToFile(FormattedMessage, *LogFilePath, FFileHelper::EEncodingOptions::AutoDetect, &IFileManager::Get(), FILEWRITE_Append);
+	}
+}
+
+void FModLogUtility::LogMessage(const FString& Message, ELogLevel Level)
+{
+	int32 CurrentLogLevel = CVarLogLevel.GetValueOnAnyThread();
+	if (static_cast<int32>(Level) >= CurrentLogLevel)
+	{
+		FString FormattedMessage;
+		#if MODLOG_CLEAN_LOG
+				FormattedMessage = Message; 
+		#else
+				const FString Timestamp = FDateTime::Now().ToString(TEXT("%Y-%m-%d %H:%M:%S"));
+				FString Verbosity;
+				switch (Level)
+				{
+				case ELogLevel::Error: Verbosity = TEXT("ERROR"); break;
+				case ELogLevel::Warning: Verbosity = TEXT("WARNING"); break;
+				case ELogLevel::Debug:
+				default: Verbosity = TEXT("DEBUG"); break;
+				}
+				FormattedMessage = FString::Printf(TEXT("[%s] [%s] %s"), *Timestamp, *Verbosity, *Message);
+		#endif
+
+		if (bUseCustomLogFile && !LogFilePath.IsEmpty())
+		{
+			RawLogMessage(FormattedMessage);
+		}
+		else
+		{
+			switch (Level)
+			{
+			case ELogLevel::Error:
+				UE_LOG(MODLOG_CATEGORY_NAME, Error, TEXT("%s"), *FormattedMessage);
+				break;
+			case ELogLevel::Warning:
+				UE_LOG(MODLOG_CATEGORY_NAME, Warning, TEXT("%s"), *FormattedMessage);
+				break;
+			case ELogLevel::Debug:
+			default:
+				UE_LOG(MODLOG_CATEGORY_NAME, Log, TEXT("%s"), *FormattedMessage);
+				break;
+			}
+		}
+	}
+}
+
+void UModLog::UseCustomLogFile(bool bEnableCustomLogFile)
+{
+	FModLogUtility::Get().SetUseCustomLogFile(bEnableCustomLogFile);
+}
+
+void UModLog::LogMessage(const FString& Message, EBPLogLevel Level)
+{
+	FModLogUtility& Logger = FModLogUtility::Get();
+	Logger.LogMessage(Message, static_cast<ELogLevel>(Level));
+}
+
+void UModLog::InitializeLoggingModule()
+{
+	FModLogUtility::Get().InitializeLog();
+	FModLogUtility::Get().LogMessage(FString::Printf(TEXT("%s Logging initialized"), MODLOG_MOD_NAME), ELogLevel::Debug);
+}
+
+void UModLog::ShutdownLoggingModule()
+{
+	FModLogUtility::Get().ShutdownLog();
+}
---
+++ ModLogUtility.h
@@ -0,0 +1,108 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Misc/FileHelper.h"
+#include "HAL/PlatformFilemanager.h"
+#include "ModLogUtility.generated.h"
+
+///////////////////////////////////////////////////////////////////////////
+/// All the configuration is done with these defines - you need to put	///
+/// your mod information in here. Don't forget to update the MYMOD_API	///
+/// down below as well													///
+///////////////////////////////////////////////////////////////////////////
+#define MODLOG_CATEGORY_NAME ModLog
+#define MODLOG_CONSOLE_VARIABLE_NAME "ModLog.LogLevel"
+#define MODLOG_DIRECTORY "FactoryGame/Saved/Logs"
+#define MODLOG_FILE_NAME TEXT("ModLog")
+#define MODLOG_MOD_NAME TEXT("DefaultMod")
+#define MODLOG_USE_CUSTOM_LOGGING true	// Set to false to disable writing to custom file for release version of mod
+#define MODLOG_CLEAN_LOG true			// Set to false to add in the timestamp and verbosity information
+
+
+// Don't change this line
+DECLARE_LOG_CATEGORY_EXTERN(MODLOG_CATEGORY_NAME, Log, All);
+
+// Maybe there's a more elegant way than duplicate enums, but this is prototype
+enum class ELogLevel
+{
+	Debug = 2,
+	Warning = 1,
+	Error = 0
+};
+UENUM(BlueprintType)
+enum class EBPLogLevel : uint8
+{
+	Error UMETA(DisplayName = "Error"),
+	Warning UMETA(DisplayName = "Warning"),
+	Debug UMETA(DisplayName = "Debug")
+};
+
+
+class FModLogUtility
+{
+public:
+	///////////////////////////////////////////////////////////////////////////
+	/// This LogMessage() is how you will log messages in C++			    ///
+	/// Example usage:														///
+	/// FModLogUtility::Get().LogMessage("Your Message", ELogLevel::Debug); ///
+	///////////////////////////////////////////////////////////////////////////
+	void LogMessage(const FString& Message, ELogLevel Level);
+
+	///////////////////////////////////////////////////////////////////////////
+	/// Don't call these directly										    ///
+	static FModLogUtility& Get();                                           ///
+	void InitializeLog();                                                   ///
+	void ShutdownLog();                                                     ///
+	void SetUseCustomLogFile(bool bEnableCustomLogFile);                    ///
+	///////////////////////////////////////////////////////////////////////////
+
+private:
+	FModLogUtility() = default;
+	FModLogUtility(const FModLogUtility&) = delete;
+	FModLogUtility& operator=(const FModLogUtility&) = delete;
+	bool bUseCustomLogFile = MODLOG_USE_CUSTOM_LOGGING;
+	FString LogFilePath;
+	FCriticalSection LogFileMutex;
+	static FModLogUtility Instance;
+
+	void RawLogMessage(const FString& Message);
+	static const FLogCategoryBase* LogCategory;
+};
+
+
+/////////////////////////////////////////////////////////////////////////
+/// CHANGE MYMOD_API HERE TO YOUR MOD'S API INSTEAD					  ///
+/////////////////////////////////////////////////////////////////////////
+UCLASS()
+class MYMOD_API UModLog : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	/////////////////////////////////////////////////////////////////////////
+	/// Start here: You need to call this InitializeLoggingModule()       ///
+	/// whether you're using C++ logging or blueprint logging             ///
+	/////////////////////////////////////////////////////////////////////////
+	UFUNCTION(BlueprintCallable, Category = MODLOG_MOD_NAME)
+	static void InitializeLoggingModule();
+
+	/////////////////////////////////////////////////////////////////////////
+	/// Called to shut-down the logging module							  ///
+	/////////////////////////////////////////////////////////////////////////
+	UFUNCTION(BlueprintCallable, Category = MODLOG_MOD_NAME)
+	static void ShutdownLoggingModule();
+
+	/////////////////////////////////////////////////////////////////////////
+	/// Set this true in Blueprint to write to custom file, or false to   ///
+	/// write to normal UE_Log output. It will override you have set      ///
+	/// here in the C++ so it's kind of the "master" control              ///
+	/////////////////////////////////////////////////////////////////////////
+	UFUNCTION(BlueprintCallable, Category = MODLOG_MOD_NAME)
+	static void UseCustomLogFile(bool bEnableCustomLogFile);
+	
+	/////////////////////////////////////////////////////////////////////////
+	/// Self-explanatory - logs messages (called in blueprints)			  ///
+	/////////////////////////////////////////////////////////////////////////
+	UFUNCTION(BlueprintCallable, Category = MODLOG_MOD_NAME)
+	static void LogMessage(const FString& Message, EBPLogLevel Level);
+};
